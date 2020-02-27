89858a3297c218a0e7e7794996d97600202991ef
Melissa <melissamnt10@gmail.com>
2020-02-27 16:47:44 -0500

    added tableau dashboard

--- code/src/main.py
+++ code/src/main.py
@@ -12,9 +12,10 @@ df_entity_raw, df_names_raw = extraction.extracting_data()
 # Preprocessing
 df_entity_clean, names_mun_clean = preprocessing.preprocessing_data(df_entity_raw, df_names_raw)
 
-#test_names = ["HUILA - ALCALDÍA MUNICIPIO DE NEIVA",
-#              "SANTANDER - ALCALDÍA MUNICIPIO DE BUCARAMANGA"]
-test_names = names_mun_clean
+test_names = ["HUILA - ALCALDÍA MUNICIPIO DE NEIVA",
+              "SANTANDER - ALCALDÍA MUNICIPIO DE BUCARAMANGA",
+              "VALLE DEL CAUCA - ALCALDÍA MUNICIPIO DE PALMIRA"]
+# test_names = names_mun_clean
 # Chain construction
 chain = string_similarity.contracting_chain(test_names, 3, df_entity_clean)
 
